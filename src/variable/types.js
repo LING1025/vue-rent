ba0d8c4416005d4aebf525bfc103dbf40b82a088
justone <2274104495@qq.com>
2020-06-02 16:24:53 +0800

    2020-06-02 second

--- src/variable/types.js
+++ src/variable/types.js
@@ -1,134 +1,4 @@
 
-/* const orgOption = [
-  { key: 1, display_name: '运务课' },
-  { key: 2, display_name: '上海区' },
-  { key: 3, display_name: '上海营业部' },
-  { key: 4, display_name: '上海营业部一课' },
-  { key: 5, display_name: '上海营业部二课' },
-  { key: 6, display_name: '上海营业部三课' },
-  { key: 7, display_name: '上海营业部四课' },
-  { key: 8, display_name: '上海短租部' },
-  { key: 9, display_name: '山东区' },
-  { key: 10, display_name: '广东区' },
-  { key: 11, display_name: '中山营业部' },
-  { key: 12, display_name: '中山营业部一课' },
-  { key: 13, display_name: '开发课' },
-  { key: 14, display_name: '车技上海课' },
-  { key: 15, display_name: '车技苏州课' },
-  { key: 16, display_name: '车技昆山课' },
-  { key: 17, display_name: '车技课' },
-  { key: 18, display_name: '车辆服务课' },
-  { key: 19, display_name: '车辆部' },
-  { key: 20, display_name: '车辆管理课' },
-  { key: 21, display_name: '长沙营业部' },
-  { key: 22, display_name: '业务处' },
-  { key: 23, display_name: '业务部' },
-  { key: 24, display_name: '业管处' },
-  { key: 25, display_name: '东莞营业一部' },
-  { key: 26, display_name: '东莞营业一部一课' },
-  { key: 27, display_name: '东莞营业一部二课' },
-  { key: 28, display_name: '东莞营业一部三课（回租）' },
-  { key: 29, display_name: '东莞营业二部' },
-  { key: 30, display_name: '东莞营业二部一课' },
-  { key: 31, display_name: '东莞营业二部专案课' },
-  { key: 32, display_name: '企划管理处' },
-  { key: 33, display_name: '会计课' },
-  { key: 34, display_name: '全公司' },
-  { key: 35, display_name: '华东营业处' },
-  { key: 36, display_name: '华东营业处联办中心' },
-  { key: 37, display_name: '华东短租营业处' },
-  { key: 38, display_name: '华南区作业管理中心' },
-  { key: 39, display_name: '华南营业处' },
-  { key: 40, display_name: '合肥营业部' },
-  { key: 41, display_name: '回租部' },
-  { key: 42, display_name: '回租部一课（苏州）' },
-  { key: 43, display_name: '回租部二课（上海）' },
-  { key: 44, display_name: '安徽区' },
-  { key: 45, display_name: '行政课' },
-  { key: 46, display_name: '行政管理中心' },
-  { key: 47, display_name: '行销企划课' },
-  { key: 48, display_name: '佛山营业部' },
-  { key: 49, display_name: '佛山营业部一课' },
-  { key: 50, display_name: '苏州二手车课' },
-  { key: 51, display_name: '苏州区' },
-  { key: 52, display_name: '苏州营业一部' },
-  { key: 53, display_name: '苏州营业一部一课' },
-  { key: 54, display_name: '苏州营业一部二课' },
-  { key: 55, display_name: '苏州营业一部三课' },
-  { key: 56, display_name: '苏州营业一部长沙课' },
-  { key: 57, display_name: '苏州营业一部南京课' },
-  { key: 58, display_name: '苏州营业二部' },
-  { key: 59, display_name: '苏州营业二部一课' },
-  { key: 60, display_name: '苏州营业二部二课' },
-  { key: 61, display_name: '苏州营业二部二课（原无锡）' },
-  { key: 62, display_name: '苏州营业二部合肥课' },
-  { key: 63, display_name: '苏州营业二部南昌课' },
-  { key: 64, display_name: '苏州营业三部' },
-  { key: 65, display_name: '苏州营业三部一课' },
-  { key: 66, display_name: '苏州营业三部二课（网约车）' },
-  { key: 67, display_name: '苏州营业三部三课（代理商）' },
-  { key: 68, display_name: '苏州营业三部三课（原扬州）' },
-  { key: 69, display_name: '苏州营业四部' },
-  { key: 70, display_name: '苏州营业四部二课（网约车）' },
-  { key: 71, display_name: '苏州营业四部三课（代理商）' },
-  { key: 72, display_name: '苏州短租部' },
-  { key: 73, display_name: '财务部会计课' },
-  { key: 74, display_name: '财务部理财课' },
-  { key: 75, display_name: '财务管理中心' },
-  { key: 76, display_name: '昆山区' },
-  { key: 77, display_name: '昆山营业二部' },
-  { key: 78, display_name: '昆山营业二部一课' },
-  { key: 79, display_name: '昆山营业二部三课' },
-  { key: 80, display_name: '昆山营业部' },
-  { key: 81, display_name: '昆山营业部一课' },
-  { key: 82, display_name: '昆山营业部二课' },
-  { key: 83, display_name: '昆山营业部三课' },
-  { key: 84, display_name: '昆山营业部五课（回租）' },
-  { key: 85, display_name: '昆山营业部四课（南通）' },
-  { key: 86, display_name: '服务部' },
-  { key: 87, display_name: '杭州企划部' },
-  { key: 88, display_name: '杭州财管部' },
-  { key: 89, display_name: '杭州资讯部' },
-  { key: 90, display_name: '杭州营业部' },
-  { key: 91, display_name: '杭州营业部一课（杭州）' },
-  { key: 92, display_name: '杭州营业部二课（宁波）' },
-  { key: 93, display_name: '杭州营业部三课（嘉兴）' },
-  { key: 94, display_name: '杭州营服部' },
-  { key: 95, display_name: '杭州新事业室' },
-  { key: 96, display_name: '经企管理中心' },
-  { key: 97, display_name: '经营企划课' },
-  { key: 98, display_name: '青岛营业部' },
-  { key: 99, display_name: '青岛营业部一课' },
-  { key: 100, display_name: '南京区' },
-  { key: 101, display_name: '南京营业部' },
-  { key: 102, display_name: '南昌营业部' },
-  { key: 103, display_name: '契约管理课' },
-  { key: 104, display_name: '总经理室' },
-  { key: 105, display_name: '债权管理课' },
-  { key: 106, display_name: '债管课' },
-  { key: 107, display_name: '浙江区' },
-  { key: 108, display_name: '授信管理课' },
-  { key: 109, display_name: '深圳营业部' },
-  { key: 110, display_name: '深圳营业部一课' },
-  { key: 111, display_name: '维护课' },
-  { key: 112, display_name: '营服部' },
-  { key: 113, display_name: '营管部' },
-  { key: 114, display_name: '厦门营业部' },
-  { key: 115, display_name: '厦门营业部一课' },
-  { key: 116, display_name: '惠州营业部' },
-  { key: 117, display_name: '惠州营业部一课' },
-  { key: 118, display_name: '董事長室' },
-  { key: 119, display_name: '数据管理中心' },
-  { key: 120, display_name: '福州营业部' },
-  { key: 121, display_name: '福州营业部一课' },
-  { key: 122, display_name: '福建区' },
-  { key: 123, display_name: '解约管理课' },
-  { key: 124, display_name: '管理处' },
-  { key: 125, display_name: '管理部人事课' },
-  { key: 126, display_name: '管理部行政课' },
-  { key: 127, display_name: '稽核室' }
-]*/
-
 const titleOption = [
   { key: 1, display_name: '董事长' },
   { key: 2, display_name: '总经理' },
@@ -146,35 +16,43 @@ const titleOption = [
   { key: 14, display_name: '司机' }
 ]
 
-/* const groupOption = [
-  { key: 1, display_name: '上海浦东长租部' },
-  { key: 2, display_name: '上海浦西长租部' },
-  { key: 3, display_name: '上海营业1课' },
-  { key: 4, display_name: '上海营业4课' },
-  { key: 5, display_name: '上海張揚站' },
-  { key: 6, display_name: '上海短租部' },
-  { key: 7, display_name: '上海管理部' },
-  { key: 8, display_name: '广州营业1课' },
-  { key: 9, display_name: '广州营业部' },
-  { key: 10, display_name: '东莞营业部一课' }
-]*/
+const typeOption = [
+  { key: '1', display_name: 'A程式' },
+  { key: '2', display_name: 'B资料' },
+  { key: '3', display_name: 'C专案' },
+  { key: '4', display_name: 'D账号开通' },
+  { key: '5', display_name: 'E硬件维护' },
+  { key: '6', display_name: 'F软件维护' }
+]
 
-/* const roleOption = [
-  { key: 1, display_name: 'Admins', checked: false },
-  { key: 2, display_name: 'GPS信息维护', checked: false },
-  { key: 3, display_name: 'GPS暂借审核', checked: false },
-  { key: 4, display_name: '人事', checked: false },
-  { key: 5, display_name: '入车复核', checked: false },
-  { key: 6, display_name: '厂商维护', checked: false },
-  { key: 7, display_name: '上海业务员', checked: false },
-  { key: 8, display_name: '上海总经理', checked: false },
-  { key: 9, display_name: '上海营业区经理', checked: false },
-  { key: 10, display_name: '上海营业课组长', checked: false }
-]*/
+const statusOption = [
+  { key: '1', display_name: '需处理' },
+  { key: '2', display_name: '未完成' },
+  { key: '3', display_name: '已完成' }
+]
 
+const modeOption = [
+  { key: '1', display_name: '1.新增' },
+  { key: '2', display_name: '2.修改' },
+  { key: '3', display_name: '3.报表' }
+]
+
+const reasonOption = [
+  { key: '1', display_name: '1.作业需求' },
+  { key: '2', display_name: '2.客户要求' },
+  { key: '3', display_name: '3.人为疏失' }
+]
+
+const degreeOption = [
+  { key: '1', display_name: '1.急件' },
+  { key: '2', display_name: '2.重要' },
+  { key: '3', display_name: '3.普通' }
+]
 export default {
-  // orgOption,
-  titleOption
-  // groupOption
-  // roleOption
+  titleOption,
+  typeOption,
+  statusOption,
+  modeOption,
+  reasonOption,
+  degreeOption
 }
--- src/views/staff/requestForm.vue
+++ src/views/staff/requestForm.vue
@@ -1,9 +1,245 @@
 <template>
-  <div></div>
+  <div>
+    <el-container>
+      <el-header>
+        <el-row>
+          <el-col :span="6">
+            <el-button style="margin-left: 10px;" type="primary" plain icon="el-icon-edit" @click="handleCreate">新建</el-button>
+          </el-col>
+          <el-col :span="3">
+            <el-select v-model="listQuery.type" clearable placeholder="==请选择==" style="width: 100%;" @keyup.enter.native="handleFilter" >
+              <el-option v-for="item in typeOptions" :key="item.key" :label="item.display_name" :value="item.key"/>
+            </el-select>
+          </el-col>
+          <el-col :span="3">
+            <el-select v-model="listQuery.status" clearable placeholder="请选择状态" style="width: 100%;" @keyup.enter.native="handleFilter" >
+              <el-option v-for="item in statusOptions" :key="item.key" :label="item.display_name" :value="item.key"/>
+            </el-select>
+          </el-col>
+          <el-col :span="3">
+            <el-select v-model="listQuery.orgType" clearable placeholder="部门" style="width: 100%;" @keyup.enter.native="handleFilter" />
+          </el-col>
+          <el-col :span="3">
+            <el-select v-model="listQuery.userAuto" clearable placeholder="姓名" style="width: 100%;" @keyup.enter.native="handleFilter" />
+          </el-col>
+          <el-col :span="6">
+            <el-button type="primary" plain icon="el-icon-search" @click="handleFilter">查询</el-button>
+          </el-col>
+        </el-row>
+      </el-header>
+      <el-main>
+        <el-table v-loading="listLoading" :data="list" stripe border fit min style="width: 100%">
+          <el-table-column align="center" label="需求单号" prop="iTRequirementAuto" />
+          <el-table-column align="center" label="申请人" prop="userAuto" />
+          <el-table-column align="center" label="部门" prop="orgType" />
+          <el-table-column align="center" label="分类" prop="typeMode" />
+          <el-table-column align="center" label="标题" prop="subject" />
+          <el-table-column align="center" label="申请日期" prop="cDT" />
+          <el-table-column align="center" label="进度" prop="status" />
+          <el-table-column align="center" label="负责人" prop="handler" />
+          <el-table-column align="center" label="操作" fixed="right" width="360">
+            <template slot-scope="{row}">
+              <el-button type="info" plain size="small" icon="el-icon-edit" @click="handleUpdate(row)">编辑</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+
+        <!--  分页条  -->
+        <pagination v-show="total>0" :total="total" :page.sync="listQuery.pageNum" :limit.sync="listQuery.pageSize" @pagination="getList" />
+        <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible" :close-on-click-modal="false" :close-on-press-escape="false">
+          <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="100px" style="width: 500px; margin-left:50px;" >
+            <el-form-item label="申请人" prop="userAuto">
+              <el-input v-model="temp.userAuto" placeholder="请输入申请人" maxlength="30" clearable oninput="value" />
+            </el-form-item>
+            <el-form-item label="编号" prop="iTRequirementAuto">
+              <el-input v-model="temp.iTRequirementAuto" placeholder="请输入编号" maxlength="30" clearable oninput="value" />
+            </el-form-item>
+            <el-form-item label="部门" prop="orgType">
+              <el-input v-model="temp.orgType" placeholder="请输入部门" maxlength="30" clearable oninput="value" />
+            </el-form-item>
+            <el-form-item label="申请日期" prop="cDT">
+              <el-input v-model="temp.cDT" placeholder="请输入申请日期" maxlength="30" clearable oninput="value" />
+            </el-form-item>
+            <el-form-item label="标题" prop="subject">
+              <el-input v-model="temp.subject" placeholder="请输入标题" maxlength="30" clearable oninput="value" />
+            </el-form-item>
+            <el-form-item label="希望完成日" prop="expectDT">
+              <el-date-picker v-model="temp.expectDT" clearable type="date" placeholder="请选择希望完成日" style="width: 100%" />
+            </el-form-item>
+            <el-form-item label="分类" prop="typeMode">
+              <el-select v-model="temp.type" placeholder="==请选择==" style="width: 100%;">
+                <el-option v-for="item in typeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
+              </el-select>
+              <el-select v-model="temp.mode" placeholder="==请选择==" style="width: 100%;">
+                <el-option v-for="item in modeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
+              </el-select>
+            </el-form-item>
+            <el-form-item label="异动原因" prop="reason">
+              <el-select v-model="temp.reason" placeholder="==请选择==" style="width: 100%;">
+                <el-option v-for="item in reasonOptions" :key="item.key" :label="item.display_name" :value="item.key" />
+              </el-select>
+            </el-form-item>
+            <el-form-item label="程度" prop="degree">
+              <el-select v-model="temp.degree" placeholder="==请选择==" style="width: 100%;">
+                <el-option v-for="item in degreeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
+              </el-select>
+            </el-form-item>
+            <el-form-item label="内容" prop="contents">
+              <el-input type="textarea" :rows="8" v-model="temp.subject" placeholder="请输入内容" maxlength="500" clearable show-word-limit oninput="value" />
+            </el-form-item>
+            <el-form-item label="附件" prop="fileName">
+              <el-upload
+                class="upload-demo"
+                action="https://jsonplaceholder.typicode.com/posts/"
+                :on-change="handleChange">
+                <el-button size="small" type="primary">点击上传</el-button>
+              </el-upload>
+            </el-form-item>
+            <el-form-item label="签核建议" prop="memo">
+              <el-input type="textarea" :rows="3" v-model="temp.memo" placeholder="请输入签核建议" maxlength="100" clearable show-word-limit oninput="value" />
+            </el-form-item>
+            <!--<el-form-item label="签核列表" prop="flowList">
+              <el-input type="textarea" :rows="3" v-model="temp.flowList" placeholder="请输入签核列表" maxlength="30" clearable oninput="value" />
+            </el-form-item>-->
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">
+              取消
+            </el-button>
+            <el-button type="primary" @click="dialogStatus==='create'?createData():updateData()">
+              保存
+            </el-button>
+          </div>
+        </el-dialog>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 <script>
+import { currentDate } from '../../utils/dateSplice' // 获取当天日期
+import option from '@/variable/types'
+
 export default {
-  name: 'StaffRequestForm'
+  name: 'StaffRequestForm',
+  data() {
+    return {
+      agent: '',
+      total: 0,
+      list: null,
+      listLoading: true,
+      dialogFormVisible: false,
+      dialogStatus: '',
+      textMap: {
+        update: '编辑',
+        create: '新增'
+      },
+      typeOptions: option.typeOption,
+      statusOptions: option.statusOption,
+      modeOptions: option.modeOption,
+      reasonOptions: option.reasonOption,
+      degreeOptions: option.degreeOption,
+      temp: {
+        iTRequirementAuto: undefined,
+        userAuto: '',
+        type: '',
+        mode: '',
+        expectDT: '',
+        contents: '',
+        status: '',
+        flowList: '',
+        reason: '',
+        degree: '',
+        subject: '',
+        // memo: '',
+        fileName: '',
+        cDT: '',
+        typeMode: '',
+        orgType: ''
+      },
+      listQuery: {
+        type: '',
+        status: '',
+        orgType: '',
+        userAuto: '',
+        pageNum: 1,
+        pageSize: 20
+      },
+      rules: {
+        userAuto: [{ required: true, message: '申请人必填', trigger: 'change' }],
+        iTRequirementAuto: [{ required: true, message: '编号必填', trigger: 'change' }],
+        orgType: [{ required: true, message: '部门必填', trigger: 'change' }],
+        cDT: [{ required: true, message: '申请日期必填', trigger: 'change' }]
+      }
+    }
+  },
+  created() {
+    this.getList()
+  },
+  methods: {
+    getList() {
+    },
+    handleChange(file, fileList) {
+      this.fileList = fileList.slice(-3)
+    },
+    resetTemp() {
+      this.temp = {
+        iTRequirementAuto: undefined,
+        userAuto: '',
+        type: '',
+        mode: '',
+        expectDT: '',
+        contents: '',
+        status: '',
+        flowList: '',
+        reason: '',
+        degree: '',
+        subject: '',
+        // memo: '',
+        fileName: '',
+        cDT: '',
+        typeMode: '',
+        orgType: ''
+      }
+    },
+    /** 新建 */
+    handleCreate() {
+      this.resetTemp()
+      this.dialogStatus = 'create'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    /** 编辑 */
+    handleUpdate(row) {
+      this.temp = Object.assign({}, row)
+      this.dialogStatus = 'update'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    handleFilter() {
+      this.getList()
+    },
+    /** 新建 */
+    createData() {
+      this.$refs['dataForm'].validate((valid) => {
+        // eslint-disable-next-line no-empty
+        if (valid) {
+          this.temp.cDT = currentDate()
+        }
+      })
+    },
+    /** 修改 */
+    updateData() {
+      this.$refs['dataForm'].validate((valid) => {
+        // eslint-disable-next-line no-empty
+        if (valid) {
+        }
+      })
+    }
+  }
 }
 </script>
 
