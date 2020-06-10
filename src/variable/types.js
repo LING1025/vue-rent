52b47fc25542743b9b5838706df89b74d880ed6e
justone <2274104495@qq.com>
2020-06-10 10:09:40 +0800

    2020-06-10 first

--- src/variable/types.js
+++ src/variable/types.js
@@ -2,18 +2,19 @@
 const titleOption = [
   { key: 1, display_name: '董事长' },
   { key: 2, display_name: '总经理' },
-  { key: 3, display_name: '副总经理' },
-  { key: 4, display_name: '总监' },
-  { key: 5, display_name: '协理' },
-  { key: 6, display_name: '经理' },
-  { key: 7, display_name: '副理' },
-  { key: 8, display_name: '襄理' },
-  { key: 9, display_name: '主任' },
-  { key: 10, display_name: '组长' },
-  { key: 11, display_name: '专员' },
+  { key: 3, display_name: '总监' },
+  { key: 4, display_name: '经理' },
+  { key: 5, display_name: '襄理' },
+  { key: 6, display_name: '组长' },
+  { key: 7, display_name: '专员' },
+  { key: 8, display_name: '助理' },
+  { key: 9, display_name: '司机' },
+  { key: 10, display_name: '协理' },
+  { key: 11, display_name: '副理' },
   { key: 12, display_name: '业务员' },
-  { key: 13, display_name: '助理' },
-  { key: 14, display_name: '司机' }
+  { key: 13, display_name: '主任' },
+  { key: 14, display_name: '副总经理' }
+
 ]
 
 const typeOption = [
