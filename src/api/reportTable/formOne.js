673fd72f1e6570aa621a4790b35beddf45ba54d8
justone <2274104495@qq.com>
2020-06-11 15:03:02 +0800

    2020-06-11 first

--- src/api/reportTable/formOne.js
+++ src/api/reportTable/formOne.js
@@ -1,5 +1,10 @@
 import request from '@/utils/request'
 
+/**
+ * 获取部门营业报表信息
+ * @param query
+ * @returns {AxiosPromise}
+ */
 export function getOne(query) {
   return request({
     url: '/table/query',
@@ -9,6 +14,22 @@ export function getOne(query) {
       month: query.month
       /* startDate: query.startDate,
       endDate: query.endDate*/
+    }
+  })
+}
+
+/**
+ * 获取课营业报表信息
+ * @param query
+ * @returns {AxiosPromise}
+ */
+export function getTwo(query) {
+  return request({
+    url: '/table/queryke',
+    method: 'get',
+    params: {
+      year: query.year,
+      month: query.month
     }
   })
 }
