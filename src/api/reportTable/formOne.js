2be6b6313744aa56fafc639db59599cd9f4e916b
justone <2274104495@qq.com>
2020-06-30 13:42:12 +0800

    删除报表无用api

--- src/api/reportTable/formOne.js
+++ src/api/reportTable/formOne.js
@@ -24,39 +24,6 @@ export function getMode(query) {
  * @param query
  * @returns {AxiosPromise}
  */
-export function getTrail(query) {
-  return request({
-    url: '/table/queryTrail',
-    method: 'get',
-    params: {
-      userAuto: query.userAuto,
-      startDate: query.startDate,
-      endDate: query.endDate,
-      orgAuto: query.orgAuto,
-      orgUpAuto: query.orgUpAuto
-    }
-  })
-}
-
-/**
- * 获取上个月营业报表
- * @param query
- * @returns {AxiosPromise}
- */
-export function getLastMonth(query) {
-  return request({
-    url: '/table/queryLastMonth',
-    method: 'get',
-    params: {
-      userAuto: query.userAuto,
-      startDate: query.startDate,
-      endDate: query.endDate,
-      orgAuto: query.orgAuto,
-      orgUpAuto: query.orgUpAuto
-    }
-  })
-}
-
 export function getMonth(query) {
   return request({
     url: '/table/query',
