6125938dfd2f1785c0e4f8472443c08d508d7f1c
justone <2274104495@qq.com>
2020-07-13 15:29:05 +0800

    去掉初始页跳转

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -15,11 +15,11 @@
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
-          <router-link to="/">
+          <!--<router-link to="/">
             <el-dropdown-item>
               初始页
             </el-dropdown-item>
-          </router-link>
+          </router-link>-->
           <!--<a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
             <el-dropdown-item>Github</el-dropdown-item>
           </a>
