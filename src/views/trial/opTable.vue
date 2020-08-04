2411433dc2fd2e0183e628acec71dc9f612f087c
justone <2274104495@qq.com>
2020-08-04 13:59:04 +0800

    营业报表合计修改

--- src/views/trial/opTable.vue
+++ src/views/trial/opTable.vue
@@ -209,6 +209,33 @@ export default {
           sums[index] = '合计' // 第一列显示 合计
           return
         }
+        if (index === 3) {
+          if (sums[1] === 0) {
+            sums[index] = 0 + '%'
+          } else {
+            sums[index] = ((sums[2] / sums[1]) * 100).toFixed(0) + '%'
+          }
+          console.log('sums[3]')
+          console.log(sums[index])
+        }
+        if (index === 6) {
+          if (sums[4] === 0) {
+            sums[index] = 0 + '%'
+          } else {
+            sums[index] = ((sums[5] / sums[4]) * 100).toFixed(0) + '%'
+          }
+          console.log('sums[6]')
+          console.log(sums[index])
+        }
+        if (index === 9) {
+          if (sums[7] === 0) {
+            sums[index] = 0 + '%'
+          } else {
+            sums[index] = ((sums[8] / sums[7]) * 100).toFixed(0) + '%'
+          }
+          console.log('sums[9]')
+          console.log(sums[index])
+        }
         const values = (data || []).map(item => Number(item[columns.property]))
         if (!values.every(value => isNaN(value))) {
           sums[index] = values.reduce((prev, curr) => {
@@ -219,21 +246,13 @@ export default {
               return prev
             }
           }, 0)
-        } else {
-          if (index === 3) {
-            sums[index] = ((sums[2] / sums[1]) * 100).toFixed(0) + '%'
-          }
-          if (index === 6) {
-            sums[index] = ((sums[5] / sums[4]) * 100).toFixed(0) + '%'
-          }
-          if (index === 9) {
-            sums[index] = ((sums[8] / sums[7]) * 100).toFixed(0) + '%'
-          }
         }
         // setTimeout(function() {
         //   console.log('定时器打印')
         // }, 1000) // 定时器延迟1秒
       })
+      console.log('合计')
+      console.log(sums)
       return sums // 最后返回合计行的数据
       // clearTimeout(this.timer) // 监听日期输入框
     },
