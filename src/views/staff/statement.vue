49618f0901f5ead29e32de8ead18e3e4bb78f791
LING1025 <2274104495@qq.com>
2020-10-23 11:47:14 +0800

    保有台数页面

--- src/views/staff/statement.vue
+++ src/views/staff/statement.vue
@@ -4,10 +4,10 @@
       <el-header>
         <el-row>
           <el-col :span="4">
-            <el-date-picker v-model="orderQuery.startDate" type="date" value-format="yyyy-MM-dd" placeholder="请选择开始日期" style="width: 100%" @keyup.enter.native="handleFilter" /><!--使用format指定输入框的格式；使用value-format指定绑定值的格式。-->
+            <el-date-picker v-model="cusQuery.startDate" type="date" value-format="yyyy-MM-dd" placeholder="请选择开始日期" style="width: 100%" @keyup.enter.native="handleFilter" /><!--使用format指定输入框的格式；使用value-format指定绑定值的格式。-->
           </el-col>
           <el-col :span="4">
-            <el-date-picker v-model="orderQuery.endDate" type="date" value-format="yyyy-MM-dd" placeholder="请选择结束日期" style="width: 100%" @keyup.enter.native="handleFilter" /><!--使用format指定输入框的格式；使用value-format指定绑定值的格式。-->
+            <el-date-picker v-model="cusQuery.endDate" type="date" value-format="yyyy-MM-dd" placeholder="请选择结束日期" style="width: 100%" @keyup.enter.native="handleFilter" /><!--使用format指定输入框的格式；使用value-format指定绑定值的格式。-->
           </el-col>
           <el-col :span="4">
             <el-button type="primary" plain icon="el-icon-search" @click="handleFilter">查询</el-button>
@@ -26,11 +26,11 @@
           style="width: 100%"
         >
           <el-table-column align="center" label="保有客户台数（展期）" prop="tableName" />
-          <el-table-column align="center" label="前月保有客户台数" prop="lmCusNum" />
-          <el-table-column align="center" label="新增业绩台数" prop="createNum" />
-          <el-table-column align="center" label="结清期满解约" prop="endNum" />
-          <el-table-column align="center" label="结清提前解约" prop="beforeEndNum" />
-          <el-table-column align="center" label="本月保有客户台数" prop="tmCusNum" />
+          <el-table-column align="center" label="前月保有客户台数" prop="lmCusNumN" />
+          <el-table-column align="center" label="新增业绩台数" prop="createNumN" />
+          <el-table-column align="center" label="结清-期满解约" prop="endNumN" />
+          <el-table-column align="center" label="结清-提前解约" prop="beforeEndNumN" />
+          <el-table-column align="center" label="本月保有客户台数" prop="tmCusNumN" />
         </el-table>
       </el-main>
     </el-container>
@@ -38,7 +38,7 @@
 </template>
 <script>
 import { currentDate, dateTostring, format, getCurrentMonthFirst } from '../../utils/dateSplice'
-
+import { getCustomerNum } from '../../api/reportTable/formThree'
 export default {
   name: 'StaffStatement',
   data() {
@@ -46,7 +46,7 @@ export default {
       total: 0,
       tableData: null,
       listLoading: true,
-      orderQuery: {
+      cusQuery: {
         startDate: getCurrentMonthFirst(),
         endDate: currentDate()
       }
@@ -57,11 +57,18 @@ export default {
   },
   methods: {
     queryDouble() {
-      this.orderQuery.startDate = format(dateTostring(this.orderQuery.startDate))
-      this.orderQuery.endDate = format(dateTostring(this.orderQuery.endDate))
+      this.cusQuery.startDate = format(dateTostring(this.cusQuery.startDate))
+      this.cusQuery.endDate = format(dateTostring(this.cusQuery.endDate))
     },
     getList() {
       this.queryDouble()
+      getCustomerNum(this.cusQuery).then(response => {
+        this.tableData = response.data
+        this.total = response.data.total
+        this.listLoading = false
+      }).catch(() => {
+        this.listLoading = false
+      })
     },
     handleFilter() {
       this.getList()
