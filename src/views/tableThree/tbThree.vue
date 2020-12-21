d46cf90c892c83ff0826a4a3d6de29392c38b227
LING1025 <2274104495@qq.com>
2020-12-21 12:01:11 +0800

    导出表格

--- src/views/tableThree/tbThree.vue
+++ src/views/tableThree/tbThree.vue
@@ -16,6 +16,10 @@
           <el-col :span="4">
             <el-button type="primary" plain icon="el-icon-search" @click="handleFilter">查询</el-button>
           </el-col>
+          <div class="export">
+            <el-button style="margin-top: 2px;" type="primary" @click="exportExcel">导出EXCEL
+            </el-button>
+          </div>
         </el-row>
       </el-header>
       <el-main>
@@ -43,6 +47,9 @@
 <script>
 import option from '@/variable/types'
 import { getYearList } from '../../api/reportTable/formThree'
+import XLSX from 'xlsx'
+import FileSaver from 'file-saver'
+import { currentDate } from '../../utils/dateSplice'
 
 export default {
   name: 'TableThreeTbThree',
@@ -74,6 +81,17 @@ export default {
     },
     handleFilter() {
       this.getList()
+    },
+    exportExcel() {
+      var xlsxParam = { raw: true } // 导出的内容只做解析，不进行格式转换
+      var wb = XLSX.utils.table_to_book(document.querySelector('#tableOne'), xlsxParam)
+      var wbout = XLSX.write(wb, { bookType: 'xlsx', bookSST: true, type: 'array' })
+      try {
+        FileSaver.saveAs(new Blob([wbout], { type: 'application/octet-stream' }), currentDate() + '_' + '格上新增呆帳&回收报表.xlsx')
+      } catch (e) {
+        if (typeof console !== 'undefined') console.log(e, wbout)
+      }
+      return wbout
     }
   }
 }
