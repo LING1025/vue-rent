2955e1785d4841dda80d1ca4e38e7a47b939b392
LING1025 <2274104495@qq.com>
2021-05-17 10:08:58 +0800

    专案明细页面未完成

--- src/views/tableThree/tbFour.vue
+++ src/views/tableThree/tbFour.vue
@@ -19,7 +19,52 @@
           </el-col>
         </el-row>
       </el-header>
-      <el-main />
+      <el-main>
+        <el-table
+          id="tableOne"
+          v-loading="listLoading"
+          :data="list"
+          :header-cell-style="{background:'#336699',color:'#FFFFFF'}"
+          stripe
+          border
+          fit
+          min
+          style="width: 100%"
+        >
+          <el-table-column align="center" label="试算单号" prop="ordersAuto" />
+          <el-table-column align="center" label="客户名称" prop="customer" />
+          <el-table-column align="center" label="部门" prop="depName" />
+          <el-table-column align="center" label="业代" prop="ydName" />
+          <el-table-column align="center" label="周期" prop="weekNo" />
+          <el-table-column align="center" label="状态" prop="orderStatusN" />
+          <el-table-column align="center" label="公司别" prop="incName" />
+          <el-table-column align="center" label="厂牌" prop="brandName" />
+          <el-table-column align="center" label="车型" prop="clasenName" />
+          <el-table-column align="center" label="案件别" prop="postTypeN" />
+          <el-table-column align="center" label="车辆来源" prop="carSourceN" />
+          <el-table-column align="center" label="业务类别" prop="orderTypeN" />
+          <el-table-column align="center" label="客户来源" prop="custSourceN" />
+          <el-table-column align="center" label="月租金" prop="mAmt" />
+          <el-table-column align="center" label="期数" prop="mm" />
+          <el-table-column align="center" label="建档日" prop="createDate" />
+          <el-table-column align="center" label="专案名" prop="projectName" />
+          <el-table-column align="center" label="保证金比例" prop="bzjRate" />
+          <el-table-column align="center" label="成交利率" prop="rentRate" />
+          <el-table-column align="center" label="客户评级" prop="incLev" />
+          <el-table-column align="center" label="企业类别" prop="cusTypeN" />
+          <el-table-column align="center" label="成立日期" prop="comEstDate" />
+          <el-table-column align="center" label="注册资本额" prop="comMoney" />
+          <el-table-column align="center" label="产业类别" prop="industryCodeN" />
+          <el-table-column align="center" label="设立地址" prop="createAddress" />
+          <el-table-column align="center" label="负责人" prop="bossName" />
+          <el-table-column align="center" label="年龄" prop="bossAge" />
+          <el-table-column align="center" label="籍贯" prop="bossNativePlace" />
+          <el-table-column align="center" label="学历" prop="bossEducationN" />
+          <el-table-column align="center" label="婚姻状况" prop="bossMaritalStatusN" />
+          <el-table-column align="center" label="工作年限" prop="bossWorkingYearsN" />
+          <el-table-column align="center" label="保人(是否有)" prop="isGuarantor" />
+        </el-table>
+      </el-main>
     </el-container>
   </div>
 </template>
